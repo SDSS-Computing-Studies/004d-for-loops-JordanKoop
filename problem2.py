1423ec4f7c1217cc4dfd0a703ed8ae362a89385a
Jordan <jrobkup@gmail.com>
2021-04-16 19:19:24 -0700

    004d

--- problem2.py
+++ problem2.py
@@ -37,18 +37,17 @@ example:
 Enter a number: -4
 Invalid input
 """
-import math
 
-x= input("Enter a number:")
-x= int(x)
-factorial = 1
-
-for i in range(1,x + 1):
-    factorial = factorial*i
-
-
-x= str(x)
-y= factorial
-y= str(y)
-
-print(x + "!" + ' ' + "is" + ' ' + y)
+x=input("Enter a number: ")
+x=int(x)
+x=x+1
+r=range(1,x)
+y=1
+if x < 0:
+    print("Invalid input")
+if x > 0:
+    for z in r:
+        y=y*z
+    x=str(x-1)
+    y=str(y)
+    print(x+"!"+" "+"is"+" "+y)
